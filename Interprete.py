e4ebcea691f98e75ac427fd058ea7e4334fbc0d3
Sinuhe135 <sinuhe9999@gmail.com>
2024-04-11 01:12:20 -0700

    Excepcion si signo al final

--- Interprete.py
+++ Interprete.py
@@ -1,15 +1,27 @@
 
 def hacerOperacion(operacion):
-    if(operacion==""):
-        return 0
-        
     indiceCaracter = len(operacion)-1
     while(indiceCaracter>=0):
         if (operacion[indiceCaracter] == "+"):
+
+            if(indiceCaracter==0):
+                return hacerOperacion("0") + hacerOperacion(operacion[(indiceCaracter+1):])
+            elif(indiceCaracter==len(operacion)-1):
+                raise ValueError("Signo positivo sin valor asociado")
+            
             return hacerOperacion(operacion[:indiceCaracter]) + hacerOperacion(operacion[(indiceCaracter+1):])
+        
         if (operacion[indiceCaracter] == "-"):
+
+            if(indiceCaracter==0):
+                return hacerOperacion("0") - hacerOperacion(operacion[(indiceCaracter+1):])
+            elif(indiceCaracter==len(operacion)-1):
+                raise ValueError("Signo negativo sin valor asociado")
+            
             return hacerOperacion(operacion[:indiceCaracter]) - hacerOperacion(operacion[(indiceCaracter+1):])
+        
         indiceCaracter-=1
+        
     return int(operacion)
             
 print(hacerOperacion("-20-3+40"))
