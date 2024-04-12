c3db86751afb163771799c6b8ded4da83533908a
Sinuhe135 <sinuhe9999@gmail.com>
2024-04-12 02:18:26 -0700

    Soporte parentesis

--- Interprete.py
+++ Interprete.py
@@ -1,7 +1,21 @@
 
 def hacerOperacion(operacion):
+    corchetesAbiertos = 0 #Poner soporte de corchetes. Si se habre un corchete deja de detectar parentesis y vicecersa
+    parentesisAbiertos = 0
+    
     indiceCaracter = len(operacion)-1
     while(indiceCaracter>=0):
+
+        if (operacion[indiceCaracter] == "("): 
+            parentesisAbiertos+=1
+
+        elif (operacion[indiceCaracter] == ")"):
+            parentesisAbiertos-=1
+
+        if(parentesisAbiertos!=0):
+            indiceCaracter-=1
+            continue
+
         if (operacion[indiceCaracter] == "+"):
 
             if(indiceCaracter==0):
@@ -21,7 +35,18 @@ def hacerOperacion(operacion):
             return hacerOperacion(operacion[:indiceCaracter]) - hacerOperacion(operacion[(indiceCaracter+1):])
         
         indiceCaracter-=1
-        
+
+    #Checar parentesis sin par
+    if(parentesisAbiertos!=0):
+        raise ValueError("Parentesis sin par")
+    
+    #Eliminar parentesis
+    if(operacion[:1]=="(" and operacion[len(operacion)-1:]==")"):
+        return hacerOperacion(operacion[1:-1])
+    
+    # try:
     return int(operacion)
+    # except:
+    #     raise ValueError("Error de sintaxis")
             
-print(hacerOperacion("-20-3+40"))
+print(hacerOperacion("((5-7)-[10+4)]"))
