5d52fb951e1cc90746bcc83427e67372f28e236f
Sinuhe135 <sinuhe9999@gmail.com>
2024-04-13 03:37:15 -0700

    Multiplicacion y division

--- Interprete.py
+++ Interprete.py
@@ -3,7 +3,7 @@ def hacerOperacion(operacion):
     corchetesAbiertos = 0
     parentesisAbiertos = 0
 
-    #Multiplicacion division
+    #Suma resta
     indiceCaracter = len(operacion)-1
     while(indiceCaracter>=0):
 
@@ -49,7 +49,7 @@ def hacerOperacion(operacion):
     if(parentesisAbiertos!=0 or corchetesAbiertos!=0):
         raise ValueError("Error de agrupamiento")
     
-    #Suma resta
+    #Multiplicacion division
     indiceCaracter = len(operacion)-1
     while(indiceCaracter>=0):
 
@@ -71,25 +71,21 @@ def hacerOperacion(operacion):
             indiceCaracter-=1
             continue
 
-        if (operacion[indiceCaracter] == "+"):
+        if (operacion[indiceCaracter] == "*"):
 
-            if(indiceCaracter==0):
-                return hacerOperacion("0") + hacerOperacion(operacion[(indiceCaracter+1):])
-            elif(indiceCaracter==len(operacion)-1):
-                raise ValueError("Signo positivo sin valor asociado")
+            if(indiceCaracter==0 or indiceCaracter==len(operacion)-1):
+                raise ValueError("Signo de multiplicacion sin valor asociado")
             
-            return hacerOperacion(operacion[:indiceCaracter]) + hacerOperacion(operacion[(indiceCaracter+1):])
+            return hacerOperacion(operacion[:indiceCaracter]) * hacerOperacion(operacion[(indiceCaracter+1):])
         
-        if (operacion[indiceCaracter] == "-"):
+        if (operacion[indiceCaracter] == "/"):
 
-            if(indiceCaracter==0):
-                return hacerOperacion("0") - hacerOperacion(operacion[(indiceCaracter+1):])
-            elif(indiceCaracter==len(operacion)-1):
-                raise ValueError("Signo negativo sin valor asociado")
+            if(indiceCaracter==0 or indiceCaracter==len(operacion)-1):
+                raise ValueError("Signo de division sin valor asociado")
             
-            return hacerOperacion(operacion[:indiceCaracter]) - hacerOperacion(operacion[(indiceCaracter+1):])
+            return hacerOperacion(operacion[:indiceCaracter]) / hacerOperacion(operacion[(indiceCaracter+1):])
         
-        indiceCaracter-=1
+        indiceCaracter-=1    
     
     #Eliminar agrupamiento
     if(operacion[:1]=="(" and operacion[len(operacion)-1:]==")") or (operacion[:1]=="[" and operacion[len(operacion)-1:]=="]"):
@@ -100,4 +96,4 @@ def hacerOperacion(operacion):
     # except:
     #     raise ValueError("Error de sintaxis")
             
-print(hacerOperacion("(5-7)-[10+(4-2)]"))
+print(hacerOperacion("5-2*[2/(3+2)]"))
